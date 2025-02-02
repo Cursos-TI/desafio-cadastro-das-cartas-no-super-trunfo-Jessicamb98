92cf73700916fad344c2e69bab4086323b15a6ea
Jessicamb98 <manrichjessica@gmail.com>
2025-02-02 13:37:31 +0000

    tentando entender tudo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,38 +5,275 @@ int main() {
     printf("\033[1;36;40mSeja bem vindo ao meu primeiro jogo ;D\033[0m\n");
 
     char pais[20];
-    char estados[20];
-    char cidades[20];
+    char estados[8][20];
+    char cidades[32][20];
+    float populacao;
+    float area;
+    float pib;
+    int pturisticos;
 
     printf("Insira o nome do pais\n");
     scanf("%19s", pais);
 
-    getchar(); 
-
     printf("Insira o nome do estado \033[1mA\033[0m\n");
-    scanf("%19s", estados);
-
-    getchar();
+    scanf("%19s", estados[0]);
 
     printf("Insira o nome da Cidade \033[1mA01\033[0m\n");
-    scanf("%19s", cidades);
-    
-    getchar();
+    scanf("%19s", cidades[0]);
 
+    printf("Insira a população da cidade \033[1mA01\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA01\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA01\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA01\033[0m\n");
+    scanf("%d", &pturisticos);
+  
     printf("Insira o nome da Cidade \033[1mA02\033[0m\n");
-    scanf("%19s", cidades);
+    scanf("%19s", cidades[1]);
 
-    getchar();
+    printf("Insira a população da cidade \033[1mA02\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA02\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA02\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA02\033[0m\n");
+    scanf("%d", &pturisticos);
 
     printf("Insira o nome da Cidade \033[1mA03\033[0m\n");
-    scanf("%19s", cidades);
+    scanf("%19s", cidades[2]);
 
-    getchar();
+    printf("Insira a população da cidade \033[1mA03\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA03\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA03\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA03\033[0m\n");
+    scanf("%d", &pturisticos);
 
     printf("Insira o nome da Cidade \033[1mA04\033[0m\n");
-    scanf("%19s", cidades);
+    scanf("%19s", cidades[3]);
+
+    printf("Insira a população da cidade \033[1mA04\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA04\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA04\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA04\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mA05\033[0m\n");
+    scanf("%19s", cidades[4]);
+
+    printf("Insira a população da cidade \033[1mA05\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA05\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA05\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA05\033[0m\n");
+    scanf("%d", &pturisticos);
+    
+    printf("Insira o nome da Cidade \033[1mA06\033[0m\n");
+    scanf("%19s", cidades[5]);
+
+    printf("Insira a população da cidade \033[1mA06\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA06\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA06\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA06\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mA07\033[0m\n");
+    scanf("%19s", cidades[6]);
+
+    printf("Insira a população da cidade \033[1mA07\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA07\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA07\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA07\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mA08\033[0m\n");
+    scanf("%19s", cidades[7]);
+
+    printf("Insira a população da cidade \033[1mA08\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mA08\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mA08\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mA08\033[0m\n");
+    scanf("%d", &pturisticos);
+
+
+
+
+
+
+    printf("Insira o nome do estado \033[1mB\033[0m\n");
+    scanf("%19s", estados[1]);
+
+    printf("Insira o nome da Cidade \033[1mB01\033[0m\n");
+    scanf("%19s", cidades[0]);
+
+    printf("Insira a população da cidade \033[1mB01\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB01\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB01\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB01\033[0m\n");
+    scanf("%d", &pturisticos);
+  
+    printf("Insira o nome da Cidade \033[1mB02\033[0m\n");
+    scanf("%19s", cidades[1]);
+
+    printf("Insira a população da cidade \033[1mB02\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB02\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB02\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB02\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mB03\033[0m\n");
+    scanf("%19s", cidades[2]);
+
+    printf("Insira a população da cidade \033[1mB03\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB03\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB03\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB03\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mB04\033[0m\n");
+    scanf("%19s", cidades[3]);
+
+    printf("Insira a população da cidade \033[1mB04\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB04\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB04\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB04\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mB05\033[0m\n");
+    scanf("%19s", cidades[4]);
+
+    printf("Insira a população da cidade \033[1mB05\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB05\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB05\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB05\033[0m\n");
+    scanf("%d", &pturisticos);
+    
+    printf("Insira o nome da Cidade \033[1mB06\033[0m\n");
+    scanf("%19s", cidades[5]);
+
+    printf("Insira a população da cidade \033[1mB06\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB06\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB06\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB06\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mB07\033[0m\n");
+    scanf("%19s", cidades[6]);
+
+    printf("Insira a população da cidade \033[1mB07\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB07\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB07\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB07\033[0m\n");
+    scanf("%d", &pturisticos);
+
+    printf("Insira o nome da Cidade \033[1mB08\033[0m\n");
+    scanf("%19s", cidades[7]);
+
+    printf("Insira a população da cidade \033[1mB08\033[0m\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \033[1mB08\033[0m\n");
+    scanf( "%f", &area);
+
+    printf("Insira o PIB da cidade \033[1mB08\033[0m\n");
+    scanf("%f", &pib);
+
+    printf("Insira o número de pontos turisticos da cidade \033[1mB08\033[0m\n");
+    scanf("%d", &pturisticos);
+
+
+
+
+
+
 
     
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
